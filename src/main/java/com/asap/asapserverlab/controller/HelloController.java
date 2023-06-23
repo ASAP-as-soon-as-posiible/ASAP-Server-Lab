9f83a1e782285a4aa639ca829d644ad5cc3a5431
KWY <wodrk1001@gmail.com>
2023-06-24 01:11:43 +0900

    [feat] hello controller 구현

---
+++ src/main/java/com/asap/asapserverlab/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.asap.asapserverlab.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String getHello() {
+        return "Hello World";
+    }
+}
