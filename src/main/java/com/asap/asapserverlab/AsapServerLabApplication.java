1f949a72cf20a8d9e928f717543420d5aa991384
KWY <wodrk1001@gmail.com>
2023-06-23 14:55:50 +0900

    init project

---
+++ src/main/java/com/asap/asapserverlab/AsapServerLabApplication.java
@@ -0,0 +1,13 @@
+package com.asap.asapserverlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsapServerLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsapServerLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asap/asapserverlab/AsapServerLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asap.asapserverlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsapServerLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
